f844f5a531149222cdb5701f5332110ebe665a18
markolucic <marko_lucic_1994@yahoo.com>
2016-09-09 09:13:27 -0700

    deleted add_user_to_jobs migrations

--- db/migrate/20160907202303_add_user_to_jobs.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToJobs < ActiveRecord::Migration
-  def change
-    add_reference :jobs, :user, index: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907202303) do
+ActiveRecord::Schema.define(version: 20160907201223) do
 
   create_table "jobs", force: true do |t|
     t.string   "title"
@@ -22,10 +22,7 @@ ActiveRecord::Schema.define(version: 20160907202303) do
     t.boolean  "experience"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
   end
-
-  add_index "jobs", ["user_id"], name: "index_jobs_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "username"
